e3ef371e0b046dc234388a3aa08df81421955696
agnieszka <273017@student.pwr.edu.pl>
2025-05-11 18:42:35 +0200

    add feature: change quantity

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import './App.css'
 import ProductList from "./components/ProductList.tsx";
 import Cart from "./components/Cart.tsx";
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import {useState} from "react";
 
 interface CartItem {
@@ -11,18 +11,19 @@ interface CartItem {
     quantity: number;
 }
 
-function App() {
+const ONE_ITEM_LEFT = 1;
 
+function App() {
     const [cartItems, setCartItems] = useState<CartItem[]>([]);
     const handleAddToCart = (product: { id: number; name: string; price: string }) => {
         setCartItems(cartItems => {
             const isItemAlreadyAdded = cartItems.find(item => item.id === product.id);
             if (isItemAlreadyAdded) {
                 return cartItems.map(item =>
-                    item.id === product.id ? { ...item, quantity: item.quantity + 1 } : item
+                    item.id === product.id ? {...item, quantity: item.quantity + 1} : item
                 );
             } else {
-                return [...cartItems, { ...product, quantity: 1 }];
+                return [...cartItems, {...product, quantity: 1}];
             }
         });
     };
@@ -31,14 +32,38 @@ function App() {
         setCartItems(cartItems.filter(item => item.id !== id));
     }
 
-  return (
-      <Router>
-        <Routes>
-          <Route path="/" element={<ProductList addToCart={ handleAddToCart }/>} />
-          <Route path="/cart" element={<Cart cartItems={cartItems} removeFromCart={ handleRemoveFromCart }/>} />
-        </Routes>
-      </Router>
-  );
+    const handleIncreaseQuantity = (id: number) => {
+        setCartItems(cartItems.map(item =>
+            item.id === id ? {...item, quantity: item.quantity + 1} : item
+        ));
+    }
+
+    const handleDecreaseQuantity = (id: number) => {
+        const item = cartItems.find(item => item.id === id);
+        if (item && item.quantity === ONE_ITEM_LEFT) {
+            handleRemoveFromCart(id);
+        } else {
+            subtractOneItem(id);
+        }
+    }
+
+    function subtractOneItem(id: number) {
+        setCartItems(cartItems.map(item =>
+            item.id === id ? {...item, quantity: item.quantity - 1} : item
+        ));
+    }
+
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<ProductList addToCart={handleAddToCart}/>}/>
+                <Route path="/cart" element={<Cart cartItems={cartItems}
+                                                   removeFromCart={handleRemoveFromCart}
+                                                   increaseQuantity={handleIncreaseQuantity}
+                                                   decreaseQuantity={handleDecreaseQuantity}/>}/>
+            </Routes>
+        </Router>
+    );
 }
 
 export default App
--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -11,9 +11,11 @@ interface CartItem {
 interface CartProps {
     cartItems: CartItem[];
     removeFromCart: (id: number) => void;
+    increaseQuantity: (id: number) => void;
+    decreaseQuantity: (id: number) => void;
 }
 
-const Cart: React.FC<CartProps> = ({cartItems, removeFromCart}) => {
+const Cart: React.FC<CartProps> = ({cartItems, removeFromCart, increaseQuantity, decreaseQuantity}) => {
     return (
         <div>
             <h1>Twój koszyk</h1>
@@ -23,6 +25,11 @@ const Cart: React.FC<CartProps> = ({cartItems, removeFromCart}) => {
                     <li key={item.id}>
                         <h2>{item.name}</h2>
                         <p>Cena: {item.price} zł</p>
+                        <div className="container">
+                            <button onClick={() => decreaseQuantity(item.id)}>-</button>
+                            <p>{item.quantity}</p>
+                            <button onClick={() => increaseQuantity(item.id)}>+</button>
+                        </div>
                         <button onClick={() => removeFromCart(item.id)}>Usuń z koszyka</button>
                     </li>
                     ))}
